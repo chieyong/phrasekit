79f50d1aacd19c8270ef4b607120882e8aa6fc85
David <chieyong@gmail.com>
2026-04-12 16:46:06 +0200

    fix useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import CategoryCard from "@/components/cards/CategoryCard";
 import PhraseCard from "@/components/cards/PhraseCard";
--- src/app/phrase/[id]/page.tsx
+++ src/app/phrase/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { use, useState } from "react";
 import { notFound, useRouter } from "next/navigation";
 import Header from "@/components/layout/Header";
